b205f3add574f98a6134e5debeb2bdcc1155ea79
daylee619 <110576332+daylee619@users.noreply.github.com>
2022-12-28 17:11:49 +0900

    add: mock data 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,15 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './pages/main/Main';
 
-
 const Router = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Main />} />
-            </Routes>
-        </BrowserRouter>
-    );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Main />} />
+			</Routes>
+		</BrowserRouter>
+	);
 };
 
 export default Router;
