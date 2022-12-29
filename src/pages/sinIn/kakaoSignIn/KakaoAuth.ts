2ea4ea63063c692c003ca28ec477aaf7d892d79f
JeongHyeonSeok <hyeonseok45@gmail.com>
2022-12-29 18:52:54 +0900

    kakao 수정

--- src/pages/sinIn/kakaoSignIn/KakaoAuth.ts
+++ src/pages/sinIn/kakaoSignIn/KakaoAuth.ts
@@ -1,6 +1,4 @@
-// const CLIENT_ID = process.env.KAKAO_REST_API;
-const CLIENT_ID = 'fc2cfa8f3dec4f562e86cded40e7838a'; // rest api key
-// const REDIRECT_URI = process.env.KAKAO_REDIRECT_URI;
-const REDIRECT_URI = 'http://localhost:3000/oauth/callback/kakao';
+const CLIENT_ID = process.env.KAKAO_REST_API;
+const REDIRECT_URI = process.env.KAKAO_REDIRECT_URI;
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
